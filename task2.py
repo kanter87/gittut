e49e67cb74cf8c298a620e71a4dafbaa65372c98
Tuguldur <tuguldur@Tuguldurs-MacBook-Air.local>
2022-05-17 19:16:51 +0800

    task2

---
+++ task2.py
@@ -0,0 +1,8 @@
+print("Write a program to iterate the first 10 numbers and in each iteration, print the sum of the current and previous number. ")
+
+previous_num = 0 
+
+for i in range(1,11):
+    x_sum =previous_num+i
+    print("Current number ",i,"previus num",previous_num,"sum",x_sum)
+    previous_num=i
