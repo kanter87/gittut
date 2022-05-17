1d0b592e9b8b5d2b46e06356fd6fbc74eb8b92a1
Kanter <tuguldur@Tuguldurs-MacBook-Air.local>
2022-05-17 23:05:35 +0800

    hooson

--- task4.py
+++ task4.py
@@ -1,13 +1,10 @@
-def mytoo(num1, num2 ):
+def nn(num1, num2 ):
     multi = num1*num2
     if multi <=1000:
         return multi
     else :
         return num1+num2
 
-
-result= mytoo(30,30)
+result= nn(30,30)
 
 print("this is ",result)
-
-
