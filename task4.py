b83528ba5459772b55ce943e3d5b564d91f99c43
Tuguldur <tuguldur@Tuguldurs-MacBook-Air.local>
2022-05-17 22:27:11 +0800

    shalgah task1

---
+++ task4.py
@@ -0,0 +1,13 @@
+def mytoo(num1, num2 ):
+    multi = num1*num2
+    if multi <=1000:
+        return multi
+    else :
+        return num1+num2
+
+
+result= mytoo(30,30)
+
+print("this is ",result)
+
+
