183ff9e9abff0a5cbba6c47648ec921462ffe9a0
Kanter <tuguldur@Tuguldurs-MacBook-Air.local>
2022-05-18 11:26:29 +0800

    exercise7

---
+++ task7.py
@@ -0,0 +1,13 @@
+print("Write a program to find how many times substring “Emma” appears in the given string.")
+
+
+#str_x = ["Emma" ,"is" ,"good ","developer", "Emma","is", "a writer"]
+
+#for i in str_x:
+   # if i == "Emma":
+     #cnt= ??
+   #     print("Emma appeared",cnt,"times")
+str_x = "Emma is good developer. Emma is a writer"
+Count_Emma=str_x.count("Emma")
+print("emma appeared",Count_Emma,"times")
+
